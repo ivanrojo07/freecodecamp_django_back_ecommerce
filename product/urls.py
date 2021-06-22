2c0e907d010be36342203226043ff8f21db3dcbb
Ivan Rojo <ivanrojo07@gmail.com>
2021-06-22 18:57:57 -0500

    Create view for product show info

--- product/urls.py
+++ product/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import LatestProductList
+from .views import LatestProductList, ProductDetail
 
 urlpatterns = [
     path('latest-products/', LatestProductList.as_view()),
+    path('products/<slug:category_slug>/<slug:product_slug>/',ProductDetail.as_view()),
 ]
--- product/views.py
+++ product/views.py
@@ -1,3 +1,5 @@
+from django.http import Http404
+
 from django.shortcuts import render
 from rest_framework import serializers
 
@@ -13,4 +15,16 @@ class LatestProductList(APIView):
     def get(self,request,format=None):
         products = Product.objects.all()[0:4]
         serializer = ProductSerializer(products,many=True)
+        return Response(serializer.data)
+
+class ProductDetail(APIView):
+    def get_object(self,category_slug,product_slug):
+        try:
+            return Product.objects.filter(category__slug=category_slug).get(slug=product_slug)
+        except:
+            raise Http404
+
+    def get(self, request, category_slug, product_slug, format=None):
+        product = self.get_object(category_slug,product_slug)
+        serializer = ProductSerializer(product)
         return Response(serializer.data)
